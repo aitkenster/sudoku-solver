cf7d720fd798213e8277977fec660739043e3913
Nicola Aitken <nicola@geckoboard.com>
2016-04-20 11:25:08 +0100

    Create a grid object with rows and columns

--- solver.go
+++ solver.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"regexp"
+	"strconv"
 )
 
 var (
@@ -10,17 +11,71 @@ var (
 	ErrInvalidCharacters = errors.New("Grid contains invalid characters")
 )
 
-func isValidGrid(grid string) error {
-	if len(grid) != 81 {
+func isValidGrid(gridInput string) error {
+	if len(gridInput) != 81 {
 		return ErrIncorrectLength
 	}
-	return validChars(grid)
+	return validChars(gridInput)
 }
 
-func validChars(grid string) error {
+func validChars(gridInput string) error {
 	validChars := regexp.MustCompile(`^(\d|\s)*$`)
-	if !validChars.MatchString(grid) {
+	if !validChars.MatchString(gridInput) {
 		return ErrInvalidCharacters
 	}
 	return nil
+}
+
+type Grid struct {
+	Squares []Square
+}
+
+type Square struct {
+	Row    int
+	Column int
+	Value  int
+}
+
+func transformIntoGrid(gridInput string) (*Grid, error) {
+	g := Grid{
+		Squares: []Square{},
+	}
+	g.addSquares(gridInput)
+	g.addRowsAndColumns()
+	return &g, nil
+}
+
+func (g *Grid) addSquares(gridInput string) error {
+	for _, val := range gridInput {
+		v, err := strconv.Atoi(string(val))
+		if err != nil {
+			return ErrInvalidCharacters
+		}
+		g.Squares = append(g.Squares, Square{
+			Value: v,
+		})
+	}
+	return nil
+}
+
+func (g *Grid) addRowsAndColumns() {
+	rowNum := 0
+	for i := 0; i < len(g.Squares); i += 9 {
+		colNum := 0
+		for j := i; j < (i + 8); j++ {
+			g.addRow(j, rowNum)
+			g.addColumn(j, colNum)
+			colNum++
+		}
+		rowNum++
+	}
+	return
+}
+
+func (g *Grid) addRow(pos, rowNum int) {
+	g.Squares[pos].Row = rowNum
+}
+
+func (g *Grid) addColumn(pos, colNum int) {
+	g.Squares[pos].Column = colNum
 }
--- solver_test.go
+++ solver_test.go
@@ -26,8 +26,31 @@ func TestIsValidGrid(t *testing.T) {
 	for _, test := range tests {
 		actual := isValidGrid(test.grid)
 		if actual != test.expected {
-			t.Errorf("expected %b for grid %s, but got %b",
+			t.Errorf("expected %b for grid %#v, but got %b",
 				test.expected, test.grid, actual)
 		}
 	}
+}
+
+func TestTransformIntoGrid(t *testing.T) {
+	gridInput := "123456789123456789123456789123456789123456789123456789123456789123456789123456789"
+	grid, err := transformIntoGrid(gridInput)
+	if err != nil {
+		t.Fatalf("expected valid grid, got %#v", err)
+	}
+
+	if len(grid.Squares) != len(gridInput) {
+		t.Errorf("expected grid to contain %#v squares, instead got %#v", len(gridInput), len(grid.Squares))
+	}
+
+	expected := Square{
+		Row:    1,
+		Column: 1,
+		Value:  2,
+	}
+
+	if grid.Squares[10] != expected {
+		t.Errorf("expected square to equal %#v, instead got %#v", expected, grid.Squares[10])
+	}
+
 }
